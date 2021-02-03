0069ccaddbb0f62c2cf1780c835ccb6ee31e635f
InterdimensionalCat <bennett.thomas99@gmail.com>
2021-02-02 23:20:52 -0500

    comming back to project

--- src/Header_Files/Level/ActorUID.h
+++ src/Header_Files/Level/ActorUID.h
@@ -1,12 +1,62 @@
 #pragma once
 #include <unordered_map>
+#include <memory>
 
 namespace ic {
 	struct ActorUID {
 
+
+		static void registerNewActor(const std::string& actorname) {
+			int actorCount = (int)instanceMap.size();
+			nametoidmap.emplace(actorname, actorCount);
+			instanceMap.emplace(actorCount, std::vector<std::shared_ptr<ActorUID>>(0));
+		}
+
+		static void removeActor(const std::string& actorname) {
+			auto actorentry = nametoidmap.find(actorname);
+			if (actorentry == nametoidmap.end()) {
+				//log the error
+				throw exception();
+			}
+			else {
+				return id->second;
+			}
+		}
+
+		static int nameToID(const std::string& actorname) {
+			auto id = nametoidmap.find(actorname);
+			if (id == nametoidmap.end()) {
+				//log the error
+				throw exception();
+			}
+			else {
+				return id->second;
+			}
+		}
+
 		//creates a new ActorUID number and garuntees that the instancenum for each
-		//actor is both unique and serialized
-		static ActorUID makeNewInstance(const int actorID) {
+		//actor is unique
+		static std::shared_ptr<ActorUID> makeNewInstance(const int actorID) {
+			auto actorInstances = instanceMap.find(actorID);
+			if (actorInstances == instanceMap.end()) {
+				//log the error
+				throw exception();
+			}
+			else {
+				auto& instancesVec = actorInstances->second;
+				int newinstancenum = instancesVec.size();
+				for (size_t i = 0; i < newinstancenum; i++) {
+					if (instancesVec.at(i).use_count() == 1) {
+						//this instance value is free to use
+						instancesVec.at(i)->actorID = actorID;
+						return instancesVec.at(i);
+					}
+				}
+
+				//no existing instance value is free
+				instancesVec.push_back(std::make_shared<ActorUID>(actorID, newinstancenum));
+			}
+
 			auto count = instanceCount.find(actorID);
 			if (count == instanceCount.end()) {
 				instanceCount.insert(std::pair<int, int>(actorID, 0));
@@ -48,6 +98,10 @@ namespace ic {
 		int instancenum;
 
 		//key is actorID, value is the next inscancenum avalible
-		static std::unordered_map<int, int> instanceCount;
+		static std::unordered_map<std::string, int> nametoidmap;
+		//static std::unordered_map<int, int> instanceCount;
+
+		//first row is actor id, second row is pointer to instanceNum
+		static std::unordered_map<int, std::vector<std::shared_ptr<ActorUID>>> instanceMap;
 	};
 }
