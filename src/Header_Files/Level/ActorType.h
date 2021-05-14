182d533673ef0f3d1c0493d614c1d9a1143822b7
InterdimensionalCat <bennett.thomas99@gmail.com>
2021-05-14 16:45:35 -0400

    begun making actor identification more robust and easier to use

---
+++ src/Header_Files/Level/ActorType.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+namespace ic {
+
+	/**
+	 * @brief                         Identification class that denotes what
+	 *                                kind of actor the object is, does not take into
+	 *                                account which instance the object is
+	*/
+	class ActorType {
+	public:
+
+	private:
+
+		/**
+		 * @brief                     The name of the actor type this identifier represents
+		*/
+		std::string actorTypeName;
+
+		/**
+		 * @brief                     ActorType also supports variants; classes
+		*/
+		std::string variantName;
+	};
+}
---
+++ src/Source_Files/Level/ActorUID.cpp
@@ -0,0 +1,6 @@
+#include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp> // generators
+#include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
+
+using UUID = boost::uuids::uuid;
+using UUIDS = boost::uuids;
