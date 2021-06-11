005eb5727b2232a3351635a705b4b4af4fdebad7
InterdimensionalCat <bennett.thomas99@gmail.com>
2021-06-11 18:01:37 -0400

    updated lists and shrunk dependancies down to 1

--- src/Source_Files/Util/StandardLogger.cpp
+++ src/Source_Files/Util/StandardLogger.cpp
@@ -6,93 +6,14 @@
 #include <string>
 #include <sstream>
 #include <fstream>
-#include <boost/date_time.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/sinks.hpp>
-#include <boost/log/common.hpp>
-#include <boost/log/core.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/attributes.hpp>
-#include <boost/log/exceptions.hpp>
-#include <boost/log/utility/setup.hpp>
-
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/utility/setup/console.hpp>
-
-#include <boost/log/attributes/scoped_attribute.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/date_time/local_time/local_time.hpp>
-
-
-namespace logging = boost::log;
-namespace src = boost::log::sources;
-namespace sinks = boost::log::sinks;
-namespace keywords = boost::log::keywords;
-namespace attrs = boost::log::attributes;
 
 using namespace ic;
 
 StandardLogger::StandardLogger() {
 
-    fs::path filepath(fs::current_path());
-    filepath /= "logs";
-
-    boost::posix_time::ptime timeLocal = boost::posix_time::second_clock::local_time();
-    boost::gregorian::date dateObj = timeLocal.date();
-
-    std::ostringstream oss;
-    oss << dateObj;
-
-    logging::add_file_log
-    (
-        //keywords::file_name = "sample_%N.log",
-        keywords::file_name = oss.str() + "_%N.log",
-        keywords::rotation_size = 10 * 1024 * 1024,
-        keywords::target = filepath.string(),
-        keywords::format = "[Thread:%ThreadID%][%TimeStamp%][%Severity%]%Message%"
-    );
-
-    logging::add_console_log(std::cout,
-        keywords::format = "[Thread:%ThreadID%][%TimeStamp%][%Severity%]%Message%"
-    );
-
-    //logging::core::get()->set_filter
-    //(
-    //    logging::trivial::severity >= logging::trivial::debug
-    //);
-
-    logging::add_common_attributes();
 }
 StandardLogger::~StandardLogger() {}
+
 void StandardLogger::log(const std::string& message, const LogSeverity severity, const LogType type) {
-    std::ostringstream oss;
-    //oss << "[" << type << "]: " << message;
-    oss << "[" << logTypeToString(type) << "]: " << message;
-    switch (severity) {
-    case LogSeverity::Trace:
-        BOOST_LOG_TRIVIAL(trace) << "[" << logTypeToString(type) << "]: " << message;
-        break;
-    case LogSeverity::Debug:
-        BOOST_LOG_TRIVIAL(debug) << "[" << logTypeToString(type) << "]: " << message;
-        break;
-    case LogSeverity::Info:
-        BOOST_LOG_TRIVIAL(info) << "[" << logTypeToString(type) << "]: " << message;
-        break;
-    case LogSeverity::Warning:
-        BOOST_LOG_TRIVIAL(warning) << "[" << logTypeToString(type) << "]: " << message;
-        break;
-    case LogSeverity::Error:
-        BOOST_LOG_TRIVIAL(error) << "[" << logTypeToString(type) << "]: " << message;
-        break;
-    case LogSeverity::Fatal:
-        BOOST_LOG_TRIVIAL(fatal) << "[" << logTypeToString(type) << "]: " << message;
-        break;
-    }
+
 }
